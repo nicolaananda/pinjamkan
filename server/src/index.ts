5312cb230af5f8350697eb64d1f85b8d1a67f418
Nicola Ananda Dwi Ervantoro <nicolaanandadwiervantoro@gmail.com>
2024-06-02 23:06:41 +0700

    Add Cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.get("/", (req, res) => res.json({ message: "Hello dari Server" }));
 
