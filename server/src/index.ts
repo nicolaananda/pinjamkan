4c4cf8eb6d57123d99ef9fc03bc89eb057d72c8e
Nicola Ananda Dwi Ervantoro <nicolaanandadwiervantoro@gmail.com>
2024-06-02 23:00:36 +0700

    Add Frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+app.get("/", (req, res) => res.json({ message: "Hello dari Server" }));
 
 app.listen(8000);
